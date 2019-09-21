4db923f075a6bdbae82f7e7b3d71004b162a73ed
kjhmh2 <1492012973@qq.com>
2019-09-22 00:38:39 +0800

    update

---
+++ Homework2/Priest and Devil/Priest and Devil/Assets/Scripts/GameObjects.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjects
+{
+    readonly GameObject Instance;
+    readonly Move Move;
+    readonly ClickGUI clickGUI;
+    readonly int type;
+
+    bool OnBoat = false;
+    Coast coast;
+
+
+    public GameObjects(string Type)
+    {
+        // is priest
+        if (Type == "priest")
+        {
+            Instance = Object.Instantiate(Resources.Load("Priest", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+            type = 0;
+        }
+        // is devil
+        else
+        {
+            Instance = Object.Instantiate(Resources.Load("Devil", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+            type = 1;
+        }
+        Move = Instance.AddComponent(typeof(Move)) as Move;
+
+        clickGUI = Instance.AddComponent(typeof(ClickGUI)) as ClickGUI;
+        clickGUI.setController(this);
+    }
+
+    public void setName(string name)
+    {
+        Instance.name = name;
+    }
+
+    public void setPosition(Vector3 position)
+    {
+        Instance.transform.position = position;
+    }
+
+    public void moveToPosition(Vector3 position)
+    {
+        Move.SetDestination(position);
+    }
+
+    public int getType()
+    {
+        return type;
+    }
+
+    public string getName()
+    {
+        return Instance.name;
+    }
+
+    public void getOnBoat(Boat boat)
+    {
+        coast = null;
+        Instance.transform.parent = boat.getGameobj().transform;
+        OnBoat = true;
+    }
+
+    public void getOnCoast(Coast Coast)
+    {
+        coast = Coast;
+        Instance.transform.parent = null;
+        OnBoat = false;
+    }
+
+    public bool isOnBoat()
+    {
+        return OnBoat;
+    }
+
+    public Coast getCoastController()
+    {
+        return coast;
+    }
+
+    public void reset()
+    {
+        Move.Reset();
+        coast = (SSDirector.getInstance().currentScenceController as Controller).fromCoast;
+        getOnCoast(coast);
+        setPosition(coast.getEmptyPosition());
+        coast.getOnCoast(this);
+    }
+}
---
+++ Homework2/Priest and Devil/Priest and Devil/Assets/Scripts/InteracteGUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interfaces;
+
+public class InteracteGUI : MonoBehaviour {
+    Objects ObjectsController;
+    static int GameState = 0;
+    public int SetState { get { return GameState; } set { GameState = value; } }
+
+	void Start () {
+        ObjectsController = SSDirector.getInstance().currentScenceController as Objects;
+    }
+
+    private void OnGUI()
+    {
+        //GameOver
+        if (GameState == 1)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 30, Screen.height / 2 - 70, 300, 300), "Gameover!");
+            if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 15, 140, 70), "Restart"))
+            {
+                GameState = 0;
+                ObjectsController.Restart();
+            }
+        }
+        //Win the Game
+        else if (GameState == 2)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 30, Screen.height / 2 - 70 , 100, 50), "Win!");
+            if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 15, 140, 70), "Restart"))
+            {
+                GameState = 0;
+                ObjectsController.Restart();
+            }
+        }
+    }
+}
