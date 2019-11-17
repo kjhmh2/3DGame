4f032a42f230e334101ea61cec4948e046fbdf7e
kjhmh2 <1492012973@qq.com>
2019-11-17 17:20:12 +0800

    udpate

---
+++ Homework8/Health Bar/Assets/Scripts/BloodBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BloodBar : MonoBehaviour
+{
+    public float health = 0.0f;
+
+    private float resultHealth;
+    private Rect HealthBar;
+
+    void Start()
+    {
+        HealthBar = new Rect(200, 40, 200, 20);
+        resultHealth = health;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 40), "Increase HP"))
+            resultHealth = resultHealth + 0.1f;
+        if (GUI.Button(new Rect(10, 70, 100, 40), "Decrease HP"))
+            resultHealth = resultHealth - 0.1f;
+        if (resultHealth > 1.0f)
+            resultHealth = 1.0f;
+        if (resultHealth < 0.0f)
+            resultHealth = 0.0f;
+
+        // make health bar change smoothly
+        health = Mathf.Lerp(health, resultHealth, 0.05f);
+
+        // show health
+        GUI.HorizontalScrollbar(HealthBar, 0.0f, health, 0.0f, 1.0f);
+    }
+}
---
+++ Homework8/Health Bar/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider mainSlider;
+    public float HP;
+
+    private void Start()
+    {
+        mainSlider = GetComponent<Slider>();
+        HP = mainSlider.maxValue;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 40), "Increase HP"))
+            HP += 10;
+        if (GUI.Button(new Rect(10, 70, 100, 40), "Decrease HP"))
+            HP -= 10;
+        if (HP > 100)
+            HP = 100;
+        if (HP < 0)
+            HP = 0;
+        mainSlider.value = HP;
+    }
+}
