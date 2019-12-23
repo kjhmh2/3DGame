9b0f59295a57346318185d2c087c4395b10bbdf9
kjhmh2 <1492012973@qq.com>
2019-12-24 01:44:42 +0800

    update

---
+++ Homework10/AR Project/Assets/Scripts/Control.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Control : MonoBehaviour
+{
+    //public GameObject vb;
+    public Animator ani;
+    public VirtualButtonBehaviour[] vbs;
+    public GameObject vb;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "button1")
+        {
+            ani.gameObject.transform.position += new Vector3(1, 0, 0);
+        }
+        else
+        {
+            ani.gameObject.transform.position += new Vector3(-1, 0, 0);
+        }
+        Debug.Log(vb.VirtualButtonName);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++ i)
+        {
+            vbs[i].RegisterOnButtonPressed(OnButtonPressed);
+            vbs[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
